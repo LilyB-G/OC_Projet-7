c358a6c3ac85eace2ba18de6efa241288c65c9ea
Lily <justpunkrock@hotmail.fr>
2022-10-23 17:50:23 +0200

    Work on admin

--- back/services/dbAction.js
+++ back/services/dbAction.js
@@ -72,6 +72,54 @@ exports.updateOneEntrie = function (table, data, condition) {
     });
 };
 
+exports.InsertOrUpdateEntries = function (table, data, condition) {
+    let keys = '';
+    let values = '';
+
+    //console.log(data);
+
+    for (let obj in data) {
+        //console.log(obj);
+
+        if (keys.length > 0) {
+            keys = keys + ',' + obj;
+        } else {
+            keys = obj;
+        };
+        if (values.length > 0) {
+            values = values + ',\'' + data[obj] + '\'';
+        } else {
+            if (typeof data[obj] != String) {
+                values = String(data[obj]);    // cas de IdUser (Number) si passé en premier => cause une erreur
+            } else {
+                values = '\'' + data[obj] + '\'';
+            }
+
+        };
+
+    };
+
+    //console.log(keys + ' | ' + values);
+
+    return new Promise((resolve, reject) => {
+        let queryString = '';
+        if (condition != '') {
+            queryString = "Insert Ignore into `" + table + "`(" + keys + ") Values (" + values + ") where " + condition + ";";
+        } else {
+            queryString = "Insert Ignore into `" + table + "`(" + keys + ") Values (" + values + ")" + ";";
+        };
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                reject(err);
+            }
+
+            resolve(result);
+        });
+    });
+};
+
+
 exports.deleteOneEntrie = function (table, condition) {
     return new Promise((resolve, reject) => {
         let queryString = "Delete from`" + table + " where " + condition + ";";
